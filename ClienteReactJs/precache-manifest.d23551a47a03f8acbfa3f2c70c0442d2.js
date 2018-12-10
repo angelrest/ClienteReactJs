a8141c49faaa007410aba4d599a2cf3bb21ee42c
angelrest <45704368+angelrest@users.noreply.github.com>
2018-12-10 18:56:12 -0500

    Vista node

---
+++ ClienteReactJs/precache-manifest.d23551a47a03f8acbfa3f2c70c0442d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f66c9fafffa7fa41d4e1",
+    "url": "/static/js/main.f66c9faf.chunk.js"
+  },
+  {
+    "revision": "d49d5dc7855b3270fb70",
+    "url": "/static/js/1.d49d5dc7.chunk.js"
+  },
+  {
+    "revision": "f66c9fafffa7fa41d4e1",
+    "url": "/static/css/main.ba0d2108.chunk.css"
+  },
+  {
+    "revision": "d49d5dc7855b3270fb70",
+    "url": "/static/css/1.348dee51.chunk.css"
+  },
+  {
+    "revision": "af4eb251ef2e2addfd521a89eda204cc",
+    "url": "/index.html"
+  }
+];
